2d9da1a7cd97c63530f2dcc4b1d9209ed0918800
Chris Fournier <chris.m.fournier@gmail.com>
2012-06-28 00:57:34 -0400

    Add micro support.

--- src/python/main/segeval/window/Pk.py
+++ src/python/main/segeval/window/Pk.py
@@ -37,11 +37,14 @@ from .. import SegmentationMetricError, compute_pairwise, \
     convert_masses_to_positions, compute_pairwise_values, create_tsv_rows
 from ..data import load_file
 from ..data.TSV import write_tsv
-from ..data.Display import render_mean_values
+from ..data.Display import render_mean_values, render_mean_micro_values
+
+
+DEFAULT_PERMUTED = True
 
 
 def pk(hypothesis_positions, reference_positions, window_size=None,
-       one_minus=False, convert_from_masses=False):
+       one_minus=False, convert_from_masses=False, return_parts=False):
     '''
     Calculates the Pk segmentation evaluation metric score for a
     hypothetical segmentation against a reference segmentation for a given
@@ -68,7 +71,7 @@ def pk(hypothesis_positions, reference_positions, window_size=None,
     .. note:: See :func:`segeval.convert_masses_to_positions` for an example of
               the input format.
     '''
-    # pylint: disable=C0103
+    # pylint: disable=C0103,R0913
     # Convert from masses into positions 
     if convert_from_masses:
         reference_positions  = convert_masses_to_positions(reference_positions)
@@ -99,7 +102,10 @@ def pk(hypothesis_positions, reference_positions, window_size=None,
     # Perform final division
     p_k = Decimal(sum_differences) / measurements
     if not one_minus:
-        return p_k
+        if return_parts:
+            return sum_differences, measurements
+        else:
+            return p_k
     else:
         return Decimal('1.0') - p_k
 
@@ -127,7 +133,47 @@ def pairwise_pk(dataset_masses, one_minus=False, convert_from_masses=True):
         return pk(hypothesis_masses, reference_masses, one_minus=one_minus,
                   convert_from_masses=convert_from_masses)
     
-    return compute_pairwise(dataset_masses, wrapper, permuted=True)
+    return compute_pairwise(dataset_masses, wrapper, permuted=DEFAULT_PERMUTED)
+
+
+def pairwise_pk_micro(dataset_masses, one_minus=False,
+                      convert_from_masses=True):
+    '''
+    Calculate mean (micro) pairwise pk.
+    
+    .. seealso:: :func:`percentage`
+    .. seealso:: :func:`segeval.compute_pairwise`
+    
+    :param dataset_masses: Segmentation mass dataset (including multiple \
+                           codings).
+    :type dataset_masses: dict
+        
+    :returns: Mean (micro)
+    :rtype: :class:`decimal.Decimal`
+    '''
+    # pylint: disable=C0103
+    def wrapper(hypothesis_masses, reference_masses, return_parts=True):
+        '''
+        Wrapper to provide parameters.
+        '''
+        return pk(hypothesis_masses, reference_masses, one_minus=False,
+                  convert_from_masses=convert_from_masses,
+                  return_parts=return_parts)
+    pairs = compute_pairwise_values(dataset_masses, wrapper,
+                                    return_parts=True)
+    
+    agree, total = 0, 0
+    for values in pairs.values():
+        cur_agree, cur_total = values
+        agree += cur_agree
+        total += cur_total
+    
+    p_k = Decimal(agree) / Decimal(total)
+    
+    if one_minus:
+        return Decimal('1.0') - p_k
+    else:
+        return p_k
 
 
 OUTPUT_NAME = 'Mean Pk'
@@ -147,7 +193,8 @@ def values_pk(dataset_masses, name, one_minus):
                   convert_from_masses=True)
     # Get values
     header = list(['coder1', 'coder2', name])
-    values = compute_pairwise_values(dataset_masses, wrapper, permuted=True)
+    values = compute_pairwise_values(dataset_masses, wrapper,
+                                     permuted=DEFAULT_PERMUTED)
     return create_tsv_rows(header, values)
 
 
@@ -155,9 +202,11 @@ def parse(args):
     '''
     Parse this module's metric arguments and perform requested actions.
     '''
+    # pylint: disable=C0103
     output = None
     values = load_file(args)[0]
     one_minus = args['oneminus']
+    micro = args['micro']
     name = SHORT_NAME
     
     if one_minus:
@@ -169,10 +218,13 @@ def parse(args):
         output_file = args['output'][0]
         header, rows = values_pk(values, name, one_minus)
         write_tsv(output_file, header, rows)
+    elif micro:
+        mean = pairwise_pk_micro(values, one_minus=one_minus)
+        output = render_mean_micro_values(name, mean)
     else:
         # Create a string to output
-        mean, std, var, stderr = pairwise_pk(values, one_minus)
-        output = render_mean_values(name, mean, std, var, stderr)
+        mean, std, var, stderr, n = pairwise_pk(values, one_minus)
+        output = render_mean_values(name, mean, std, var, stderr, n)
     return output
 
 
@@ -181,9 +233,11 @@ def create_parser(subparsers):
     Setup a command line parser for this module's metric.
     '''
     from ..data import parser_add_file_support
+    from .. import parser_micro_support
     parser = subparsers.add_parser('pk',
                                    help=OUTPUT_NAME)
     parser_add_file_support(parser)
     parser_one_minus_support(parser)
+    parser_micro_support(parser)
     parser.set_defaults(func=parse)
 
