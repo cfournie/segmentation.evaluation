f5005fd27cb896119a2cab1213751216b9dae416
Chris Fournier <chris.m.fournier@gmail.com>
2013-07-03 15:19:08 -0600

    Updated version to 2.0.3.

--- segeval/__init__.py
+++ segeval/__init__.py
@@ -11,7 +11,7 @@ from types import ModuleType
 
 
 # Package description
-__version_number__ = '2.0.2'
+__version_number__ = '2.0.3'
 __release__ = None
 __version__ = '-'.join((__version_number__, __release__)) if __release__ is not None else __version_number__
 __project__ = 'SegEval'
